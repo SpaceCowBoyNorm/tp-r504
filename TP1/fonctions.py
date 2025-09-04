68bb67494c50a7ee03bd147fe4c72eb7d762cd24
SpaceCowBoyNorm <lu.poti.cas@gmail.com>
2025-09-04 17:04:42 +0200

    foncitga

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,6 +1,19 @@
 def puissance(a, b):
 	if not type(a) is int or not type(b) is int:
 		raise TypeError("Only integers are allowed")
-	else:
-		return a**b
+	
+
+	if a == 0 and b == 0:
+		raise ValueError("0 puissance de 0 est définie")
+
+	result_puissance = 1
+
+	if b > 0:
+		for _ in range(b):
+			result_puissance *= a
+	elif b < 0:
+		for _ in range(-b):
+			result_puissance *= a
+		result_puissance = 1 / result_puissance
+	return result_puissance
 
