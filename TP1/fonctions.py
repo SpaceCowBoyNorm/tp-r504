2f1ff0abf8b2ae8a41cc6f4fe7740fe1dc24eb8e
SpaceCowBoyNorm <lu.poti.cas@gmail.com>
2025-09-04 17:22:23 +0200

    save

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,19 +1,25 @@
 def puissance(a, b):
-	if not type(a) is int or not type(b) is int:
-		raise TypeError("Only integers are allowed")
+	try:
+		if not type(a) is int or not type(b) is int:
+			raise TypeError("Only integers are allowed")
 	
 
-	if a == 0 and b == 0:
-		raise ValueError("0 puissance de 0 est définie")
+		if a == 0 and b == 0:
+			raise ValueError("0 puissance de 0 est définie")
 
-	result_puissance = 1
+		result_puissance = 1
 
-	if b > 0:
-		for _ in range(b):
-			result_puissance *= a
-	elif b < 0:
-		for _ in range(-b):
-			result_puissance *= a
-		result_puissance = 1 / result_puissance
-	return result_puissance
+		if b > 0:
+			for _ in range(b):
+				result_puissance *= a
+		elif b < 0:
+			for _ in range(-b):
+				result_puissance *= a
+			result_puissance = 1 / result_puissance
 
+
+		return result_puissance
+
+	except (TypeError, ValueError) as e:
+		print(f"Exception attrapée : {e}")
+		return None
